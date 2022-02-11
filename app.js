292c9888ab8726363610952e3a3812ae0f15e385
Oshan12345 <bsagor99@gmail.com>
2022-02-11 23:10:32 +0600

    updated

--- app.js
+++ app.js
@@ -2,90 +2,90 @@ const detailsButton = document.getElementById("detail-submit-btn");
 
 detailsButton.addEventListener("click", function () {
   const buyerDetails = document.getElementById("buyer-details-input");
-
   document.getElementById("buyer-info").innerText = buyerDetails.value;
   buyerDetails.value = "";
 });
 
 const addProductBtn = document.getElementById("add-details-btn");
 
-addProductBtn.addEventListener("click", () => {
+addProductBtn.addEventListener("click", function () {
   const infoTable = document.getElementById("info-table");
   const itemName = document.getElementById("item-name-input");
   const itemPrice = document.getElementById("item-price-input");
   const itemQuantity = document.getElementById("item-quantity-input");
- console.log(itemPrice.value);
- if (itemName.value=='' || itemPrice.value<0 || itemQuantity.value<0||itemPrice.value =='' || itemQuantity.value=="") {
-   console.log('sdasd')
-   return
- }
-  
+
+  if (
+    itemName.value == "" ||
+    itemPrice.value < 0 ||
+    itemQuantity.value < 0 ||
+    itemPrice.value == "" ||
+    itemQuantity.value == ""
+  ) {
+    console.log("sorry");
+    return;
+  }
+
+  {
+    /* <tr>
+ <th >1</th>
+ <td>Mark</td>
+<td>Otto</td>
+<td class='item-total'>@mdo</td>
+</tr> */
+  }
+
   const totalPrice = parseInt(itemPrice.value) * parseInt(itemQuantity.value);
 
-  const tr = document.createElement("tr");
-  const th = document.createElement("th");
+  //   const tr = document.createElement("tr"); //<tr></tr>
+  const tr = element("tr");
+  const th = element("th");
+
   const td1 = document.createElement("td");
   const td2 = document.createElement("td");
   const td3 = document.createElement("td");
-  // //new code
-  //  const td4 = document.createElement("td");
-  // //  end
 
-
-
-  td3.classList.add('item-total')
-  th.innerText = itemName.value;
-  td1.innerText = itemPrice.value;
+  td3.classList.add("item-total");
+  th.innerText = itemName.value; //<th >onion</th>
+  td1.innerText = itemPrice.value; //<th >1</th>
   td2.innerText = itemQuantity.value;
   td3.innerText = totalPrice;
-  
 
   tr.appendChild(th);
   tr.appendChild(td1);
   tr.appendChild(td2);
-  tr.appendChild(td3);
-  
+  tr.appendChild(td3); //<td class="item-total">35</td>
   infoTable.appendChild(tr);
-totalCalculation()
-
+  totalCalculation();
 });
 
-
-
-function p(param){
+function element(param) {
   return document.createElement(param);
 }
 
+function totalCalculation() {
+  const subTotal = calculateSubTotal();
 
-function totalCalculation(){
+  const subTotalToDisplay = document.getElementById("sub-total");
+  subTotalToDisplay.innerText = subTotal;
 
-const subtotal = calculateSubTotal()
-const subtotalToDisplay = document.getElementById("sub-total");
-subtotalToDisplay.innerText=subtotal
+  const tax = subTotal * 0.2;
 
-let tax = subtotal * 0.2;
-document.getElementById('tax').innerText=tax.toFixed(2)
-document.getElementById('grand-total').innerText= subtotal+tax
-document.getElementById('grand-total-2').innerText= subtotal+tax
-
-
+  document.getElementById("tax").innerText = tax.toFixed(2);
+  document.getElementById("grand-total").innerText = subTotal + tax;
+  document.getElementById("grand-total-2").innerText = subTotal + tax;
 }
 
+function calculateSubTotal() {
+  let subTotal = 0;
 
-function calculateSubTotal(){
+  const cost = document.getElementsByClassName("item-total");
 
-let subtotal=0
+  for (let i = 0; i < cost.length; i++) {
+    const element = cost[i]; //<td class="item-total">35</td>
+    const price = parseInt(element.innerText);
 
-let costs = document.getElementsByClassName("item-total");
+    subTotal = subTotal + price;
+  }
 
-for (let i = 0; i < costs.length; i++) {
-    const element = costs[i];
-    const price=parseInt(element.innerText)
-    subtotal= subtotal+price
+  return subTotal;
 }
-return subtotal
-}
-
-
-// new code 
-
